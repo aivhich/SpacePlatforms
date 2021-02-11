89961681b95802a6197fc51263e829c6f3735d1b
iTechnic <itech.ivanprohorevich@gmail.com>
2021-02-11 18:03:24 +0300

    1.78

--- src/Game/Nps.java
+++ src/Game/Nps.java
@@ -1,4 +1,16 @@
 package Game;
 
+import javax.swing.*;
+import java.awt.*;
+
+import static Game.MainGame.groundY;
+
 public class Nps {
+    Image image = new ImageIcon("image/Pers/nps/1/human0.png").getImage();
+    int x, y;
+    Nps(int x , int y){
+        this.x = x;
+        this.y = groundY - image.getHeight(null)-3;
+    }
+
 }
